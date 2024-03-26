0a2cecda2c1ae11f05d3ea43e0bbe68c2bc29e26
unknown <kaiomg.wow@gmail.com>
2024-03-26 20:51:21 -0300

    pull test

--- netflix_portfolio/settings.py
+++ netflix_portfolio/settings.py
@@ -146,3 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'conteudo:homeconteudo'
+
+LOGIN_URL = 'conteudo:login'
--- netflix_portfolio/urls.py
+++ netflix_portfolio/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('conteudo.urls')),
+    path('', include('conteudo.urls', namespace='conteudo')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
